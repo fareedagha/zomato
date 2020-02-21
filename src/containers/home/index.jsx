d46a0071263b1375b52d76e5eaa1cd7812da5150
fareedagha <fareedagha7440@gmail.com>
2020-02-21 12:18:20 +0500

    removed react bootsrap and added material ui

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 //import './styles.css';
-import Button from 'react-bootstrap/Button';
-
+import Button from '@material-ui/core/Button';
 class Home extends React.Component {
     render() {
       return(
           <div>
-              <Button>submit</Button>
+               <Button variant="contained">
+      Hello World
+    </Button>
               <h1>Nav bar</h1>
               <h2>slider</h2>
               <h3>zomato works</h3>
