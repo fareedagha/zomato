d30ca15afd8979eb0dc9867776e2868b1b4668a5
fareedagha <fareedagha7440@gmail.com>
2020-02-21 11:47:49 +0500

    create basic structure and intigrated react bootsrap

---
+++ src/containers/home/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+//import './styles.css';
+import Button from 'react-bootstrap/Button';
+
+class Home extends React.Component {
+    render() {
+      return(
+          <div>
+              <Button>submit</Button>
+              <h1>Nav bar</h1>
+              <h2>slider</h2>
+              <h3>zomato works</h3>
+              <h4>Footer</h4>
+
+          </div>
+
+      )
+    }
+  }
+
+  export default Home
